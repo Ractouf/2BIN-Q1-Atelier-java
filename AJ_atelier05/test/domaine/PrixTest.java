f87d6b2ae73b5e4d32f5fad87951b8de7005abfd
ractouf <francois.vandeputte@student.vinci.be>
2022-10-18 10:31:44 +0200

    Atelier 5 fin cours

---
+++ AJ_atelier05/test/domaine/PrixTest.java
@@ -0,0 +1,112 @@
+package domaine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrixTest {
+    private Prix prixAucune, prixPub, prixSolde;
+
+    @BeforeEach
+    void setUp() {
+        prixAucune = new Prix();
+        prixSolde = new Prix(TypePromo.SOLDE, 10);
+        prixPub = new Prix(TypePromo.PUB, 10);
+
+        prixAucune.definirPrix(1,20);
+        prixAucune.definirPrix(10,10);
+
+        prixPub.definirPrix(3,15);
+    }
+
+    @Test
+    @DisplayName("Test contructeur")
+    void testConstructeur() {
+        assertThrows(IllegalArgumentException.class, () -> new Prix(null, 10));
+        assertThrows(IllegalArgumentException.class, () -> new Prix(TypePromo.PUB, -2));
+    }
+
+    @Test
+    @DisplayName("Test get type promo")
+    void getTypePromo() {
+        //Vérifier que le type de la promo est null lors de l’instanciation d’un prix au moyen du
+        //constructeur sans paramètre
+        assertNull(prixAucune.getTypePromo());
+    }
+    @Test
+    @DisplayName("Test type correspond de la méthode get type promo")
+    void getTypePromoVerifierType() {
+        //Vérifier que le type de la promo correspond bien à celle passée en paramètre du
+        //constructeur
+        assertEquals(TypePromo.PUB, prixPub.getTypePromo());
+        assertEquals(TypePromo.SOLDE, prixSolde.getTypePromo());
+    }
+
+    @Test
+    @DisplayName("Test get valeur promo")
+    void getValeurPromo() {
+        //Vérifier que valeur de la promo est initialisée à 0 lors de l’instanciation d’un prix au
+        //moyen du constructeur sans paramètre
+        assertEquals(0, prixAucune.getValeurPromo());
+    }
+
+    @Test
+    @DisplayName("Test get valeur promo")
+    void getValeurPromoValeur() {
+        //Vérifier que la valeur de la promo correspond bien à celle passée en paramètre du
+        //constructeur
+        assertEquals(10, prixSolde.getValeurPromo());
+        assertEquals(10, prixPub.getValeurPromo());
+    }
+
+    @Test
+    @DisplayName("Test definir prix")
+    void definirPrix() {
+        //Définir un prix de 6 euros à partir de 10 unités pour l’attribut prixAucune et vérifier que
+        //l’ancien prix a été remplacé.
+        prixAucune.definirPrix(10,6);
+        assertEquals(6, prixAucune.getPrix(10));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test paramètre valeur pour la méthode définir prix")
+    @ValueSource(ints = {0, -10})
+    void definirPrixValeur(int valeur) {
+        //Vérifier que la méthode definirPrix lance une IllegalArgumentException si le
+        //paramètre valeur est 0 ou négatif
+        assertThrows(IllegalArgumentException.class, () -> prixAucune.definirPrix(10, valeur));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test paramètre quantité pour la méthode definir prix")
+    @ValueSource(ints = {0, -10})
+    void definirPrixQuantite(int quantite) {
+        //Vérifier que la méthode definirPrix lance une IllegalArgumentException si le
+        //paramètre quantité est 0 ou négatif
+        assertThrows(IllegalArgumentException.class, () -> prixAucune.definirPrix(quantite, 10));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test get prix")
+    @ValueSource(ints = {0, -10})
+    void getPrix(int quantite) {
+        //Vérifier que la méthode lance une IllegalArgumentException si le paramètre
+        //quantité est négatif ou nul
+        assertThrows(IllegalArgumentException.class, () -> prixAucune.getPrix(quantite));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test get prix")
+    @ValueSource(ints = {1, 5, 9}, {10, 15, 20, 25})
+    void getPrixTest(int quantite) {
+        //Vérifier que la méthode lance une IllegalArgumentException si le paramètre
+        //quantité est négatif ou nul
+        assertEquals(20, prixAucune.getPrix(1));
+    }
+}
