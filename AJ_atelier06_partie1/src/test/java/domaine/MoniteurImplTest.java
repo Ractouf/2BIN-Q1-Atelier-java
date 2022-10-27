7494f97e4371c75ee4df375a3386d4f7be22e12c
Ractouf <francois.vandeputte@student.vinci.be>
2022-10-27 12:04:53 +0200

    Début Atelier 6

---
+++ AJ_atelier06_partie1/src/test/java/domaine/MoniteurImplTest.java
@@ -0,0 +1,18 @@
+package domaine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoniteurImplTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ajouterStage() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void supprimerStage() {
+    }
+}
